b05aed87525a1d79dde1a55ccecbfcf7ec021db5
Deepak Prakash <deepakprakash2811@gmail.com>
2019-06-09 16:46:23 +0530

    icons added and listtile removed

--- lib/Models/navigation_drawer.dart
+++ lib/Models/navigation_drawer.dart
@@ -1,52 +1,167 @@
 import 'package:flutter/material.dart';
 
-
-
 class NavBar extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return new Drawer(
-    
       child: ListView(
         children: <Widget>[
           new UserAccountsDrawerHeader(
-            accountName: new Text("Deepak Prakash",style:TextStyle(color: Colors.white),),
-            accountEmail: new Text("Deepakprakash2811@gmail.com",style:TextStyle(color: Colors.white),),
+            accountName: new Text(
+              "Deepak Prakash",
+              style: TextStyle(color: Colors.white),
+            ),
+            accountEmail: new Text(
+              "Deepakprakash2811@gmail.com",
+              style: TextStyle(color: Colors.white),
+            ),
             currentAccountPicture: new CircleAvatar(
-              backgroundImage: AssetImage("assets/bg.png"),
-              radius: 40,
+              //backgroundImage: AssetImage("assets/bg.png"),
+              radius: 60,
+              child: Icon(
+                Icons.person,
+                size: 50,
+                color: Colors.white,
+              ),
             ),
-             decoration: BoxDecoration(image: new DecorationImage(
-               image: AssetImage("assets/bg.jpg"), fit: BoxFit.cover
-             )),
+          ),
+          InkWell(
+                      child: Container(
+                child: new Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+                Icon(
+                  Icons.event,
+                  size: 38,
+                  color: Colors.orange,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                ),
+                new Text("Events",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
+              ],
+            ),
+                ),
+          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+           new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+           InkWell(
+                      child: Container(
+                child: new Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+                Icon(
+                  Icons.event_available,
+                  size: 38,
+                  color: Colors.orange,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                ),
+                new Text("Join an Event",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
+              ],
+            ),
+                ),
+          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+          new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+         InkWell(
+                      child: Container(
+                child: new Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+                Icon(
+                  Icons.settings,
+                  size: 38,
+                  color: Colors.orange,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                ),
+                new Text("Settings",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
+              ],
+            ),
+                ),
+          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+           new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+            new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+         InkWell(
+                      child: Container(
+                child: new Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+                Icon(
+                  Icons.file_download,
+                  size: 38,
+                  color: Colors.orange,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                ),
+                new Text("Export My Data",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
+              ],
+            ),
+                ),
+          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+           new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+
+
+          new Divider(
+            indent: 20.0,
           ),
 
-          new ListTile(
-            title: new Text("Events "),
-           // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+         InkWell(
+                      child: Container(
+                child: new Row(
+              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                new Padding(
+                  padding: const EdgeInsets.all(5.0),
+                ),
+                Icon(
+                  Icons.exit_to_app,
+                  size: 38,
+                  color: Colors.orange,
+                ),
+                new Padding(
+                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                ),
+                new Text("SignOut",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
+              ],
             ),
-          new ListTile(
-            title: new Text("Join an Event"),
-            
+                ),
+          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
           ),
-          new ListTile(
-            title: new Text("Settings"),
-            
-          ),
-          new ListTile(
-            title: new Text("Export My Data"),
-            
-          ),
-
-          new Divider(indent: 10.0,),
-          new ListTile(
-            title: new Text("Sign Out"),
-           
-          )
+          
+        
         ],
-
       ),
     );
   }
