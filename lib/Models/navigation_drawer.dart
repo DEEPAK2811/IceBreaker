980e670efdf050dbbb8c63c614b7c537b4571ec1
Deepak Prakash <deepakprakash2811@gmail.com>
2019-06-16 03:47:22 +0530

    widget rebuild fixed

--- lib/Models/navigation_drawer.dart
+++ lib/Models/navigation_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:st_hack/ui/Uidata.dart';
 
 class NavBar extends StatelessWidget {
   @override
@@ -26,141 +27,153 @@ class NavBar extends StatelessWidget {
             ),
           ),
           InkWell(
-                      child: Container(
-                child: new Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Icon(
-                  Icons.event,
-                  size: 38,
-                  color: Colors.orange,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
-                ),
-                new Text("Events",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
-              ],
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
+                    Icons.event,
+                    size: 38,
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("Create Events",
+                      style: TextStyle(fontSize: 20.0, color: Colors.purple)),
+                ],
+              ),
             ),
-                ),
-          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
-          ),
-           new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-           InkWell(
-                      child: Container(
-                child: new Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Icon(
-                  Icons.event_available,
-                  size: 38,
-                  color: Colors.orange,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
-                ),
-                new Text("Join an Event",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
-              ],
-            ),
-                ),
-          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+            onTap: () {
+              // change app state...
+              Navigator.pop(context);
+              Navigator.pushNamed(context, Uidata.create_events);
+              // close the drawer
+            },
+            //=> Navigator.pushNamed(context, Uidata.create_events)
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
           ),
           new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-         InkWell(
-                      child: Container(
-                child: new Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Icon(
-                  Icons.settings,
-                  size: 38,
-                  color: Colors.orange,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
-                ),
-                new Text("Settings",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
-              ],
-            ),
-                ),
-          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+            padding: const EdgeInsets.all(5.0),
           ),
-          
-            new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-         InkWell(
-                      child: Container(
-                child: new Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Icon(
-                  Icons.file_download,
-                  size: 38,
-                  color: Colors.orange,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
-                ),
-                new Text("Export My Data",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
-              ],
+          InkWell(
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
+                    Icons.event_available,
+                    size: 38,
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("Available Event",
+                      style: TextStyle(fontSize: 20.0, color: Colors.purple)),
+                ],
+              ),
             ),
-                ),
-          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+            onTap: () {
+              // change app state...
+              Navigator.pop(context);
+              Navigator.pushNamed(context, Uidata.events);
+              // close the drawer
+            },
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
           ),
-           new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-
-
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+          ),
+          InkWell(
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
+                    Icons.settings,
+                    size: 38,
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("Settings",
+                      style: TextStyle(fontSize: 20.0, color: Colors.purple)),
+                ],
+              ),
+            ),
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+          ),
+          InkWell(
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
+                    Icons.file_download,
+                    size: 38,
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("Export My Data",
+                      style: TextStyle(fontSize: 20.0, color: Colors.purple)),
+                ],
+              ),
+            ),
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+          ),
           new Divider(
             indent: 10.0,
             color: Colors.orange,
             height: 8.0,
           ),
-
           new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-         InkWell(
-                      child: Container(
-                child: new Row(
-              //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                new Padding(
-                  padding: const EdgeInsets.all(5.0),
-                ),
-                Icon(
-                  Icons.exit_to_app,
-                  size: 38,
-                  color: Colors.orange,
-                ),
-                new Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
-                ),
-                new Text("SignOut",style: TextStyle(fontSize: 20.0,color: Colors.purple)),
-              ],
-            ),
-                ),
-          // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+            padding: const EdgeInsets.all(5.0),
           ),
-          
-        
+          InkWell(
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
+                    Icons.exit_to_app,
+                    size: 38,
+                    color: Colors.orange,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("SignOut",
+                      style: TextStyle(fontSize: 20.0, color: Colors.purple)),
+                ],
+              ),
+            ),
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
         ],
       ),
     );
