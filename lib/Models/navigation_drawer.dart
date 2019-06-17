a271297a4444400bb24d05a80c92d18876c9a805
Deepak Prakash <deepakprakash2811@gmail.com>
2019-06-17 21:02:14 +0530

    joined events added & style

--- lib/Models/navigation_drawer.dart
+++ lib/Models/navigation_drawer.dart
@@ -17,7 +17,7 @@ class NavBar extends StatelessWidget {
               style: TextStyle(color: Colors.white),
             ),
             currentAccountPicture: new CircleAvatar(
-              backgroundImage: AssetImage("assets/back.png"),
+              backgroundImage: AssetImage("assets/1.png"),
               radius: 60,
               child: Icon(
                 Icons.person,
@@ -26,7 +26,7 @@ class NavBar extends StatelessWidget {
               ),
             ),
              decoration: BoxDecoration(image: new DecorationImage(
-               image: AssetImage("assets/back.png"), fit: BoxFit.cover
+               image: AssetImage("assets/3.png"), fit: BoxFit.cover
 )),
           ),
           InkWell(
@@ -71,7 +71,7 @@ class NavBar extends StatelessWidget {
                     padding: const EdgeInsets.all(5.0),
                   ),
                   Icon(
-                    Icons.event_available,
+                    Icons.event_note,
                     size: 38,
                     color: Colors.black,
                   ),
@@ -103,15 +103,47 @@ class NavBar extends StatelessWidget {
                     padding: const EdgeInsets.all(5.0),
                   ),
                   Icon(
+                    Icons.event_available,
+                    size: 38,
+                    color: Colors.black,
+                  ),
+                  new Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
+                  ),
+                  new Text("Joined Events",
+                      style: TextStyle(fontSize: 20.0, color: Colors.pink)),
+                ],
+              ),
+            ),
+            onTap: () {
+              // change app state...
+              Navigator.pop(context);
+              Navigator.pushNamed(context, Uidata.event_space);
+              // close the drawer
+            },
+            // onTap: () => Navigator.pushNamed(context, UIData.dashboardRoute)
+          ),
+          new Padding(
+            padding: const EdgeInsets.all(5.0),
+          ),
+          InkWell(
+            child: Container(
+              child: new Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  new Padding(
+                    padding: const EdgeInsets.all(5.0),
+                  ),
+                  Icon(
                     Icons.settings,
                     size: 38,
-                    color: Colors.purple,
+                    color: Colors.black,
                   ),
                   new Padding(
                     padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
                   ),
                   new Text("Settings",
-                      style: TextStyle(fontSize: 20.0, color: Colors.teal)),
+                      style: TextStyle(fontSize: 20.0, color: Colors.pink)),
                 ],
               ),
             ),
@@ -129,15 +161,15 @@ class NavBar extends StatelessWidget {
                     padding: const EdgeInsets.all(5.0),
                   ),
                   Icon(
-                    Icons.file_download,
+                    Icons.featured_play_list,
                     size: 38,
-                    color: Colors.orange,
+                    color: Colors.black,
                   ),
                   new Padding(
                     padding: const EdgeInsets.fromLTRB(15.0, 0.0, 5.0, 0.0),
                   ),
                   new Text("Export My Data",
-                      style: TextStyle(fontSize: 20.0, color: Colors.black)),
+                      style: TextStyle(fontSize: 20.0, color: Colors.pink)),
                 ],
               ),
             ),
